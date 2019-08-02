a95f124e500651e4c4a559f4cac542e415348956
Zahin430 <zhprangon@mun.ca>
2019-08-02 01:48:40 -0230

    added md plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'src',
-        path: `${__dirname}/src/`
-      }
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            name: 'src',
+            path: `${__dirname}/src/`
+        }
     },
     'gatsby-transformer-remark'
-  ]
+]
 }
